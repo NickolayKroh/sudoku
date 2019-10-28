29f64361f803c7d46e0d0b9217845518eb17f08a
Kroh Nickolay <mikolka.del@gmail.com>
2019-10-28 22:07:40 +0300

    feat: task is solved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-// module.exports = 
-function solveSudoku(matrix) {
+module.exports = function solveSudoku(matrix) {
   let rowMissing = [],
       colMissing = [],
       squareMissing = [],
-      isSolved = false,
-      joints = [[],[],[],[],[],[],[],[],[]];
+      candidates = [[],[],[],[],[],[],[],[],[]];
   
   let squarePos = (i, j) => 3 * Math.floor(i/3) + Math.floor(j/3);
+
   let squareStart = (square) => {
     let startRow = Math.floor(square / 3) * 3,
         startCol = (square % 3) * 3;
 
     return [startRow, startCol];
   }
-  
+
   let getMissing = (arr) => {
     let resultArr = [];
     
@@ -23,10 +22,10 @@ function solveSudoku(matrix) {
 
     return resultArr;
   }
-  
+
   let setRowMissing = (i) => {
     rowMissing[i] = getMissing( matrix[i] );
-  
+
     if(rowMissing[i].length === 1) {
       for(let j = 0; j < 9; ++j)
         if(matrix[i][j] === 0) {
@@ -35,15 +34,19 @@ function solveSudoku(matrix) {
         }
 
       rowMissing[i] = [];
+      
+      return true;
     }
+    
+    return false;
   }
-  
+
   let setColMissing = (j) => {
     let arr = [];
-    
+  
     for(let i = 0; i < 9; ++i)
         arr.push( matrix[i][j] );
-    
+  
     colMissing[j] = getMissing(arr);
 
     if(colMissing[j].length === 1) {
@@ -54,9 +57,13 @@ function solveSudoku(matrix) {
         }
 
       colMissing[j] = [];
+    
+      return true;
     }
-  }
   
+    return false;
+  }
+
   let setSquareMissing = (square) => {
     let startRow = Math.floor(square / 3) * 3,
         startCol = (square % 3) * 3,
@@ -67,7 +74,7 @@ function solveSudoku(matrix) {
         arr.push( matrix[i][j] );
 
     squareMissing[square] = getMissing(arr);
- 
+
     if(squareMissing[square].length === 1) {
       for(let i = startRow; i < startRow + 3; ++i)
         for(let j = startCol; j < startCol + 3; ++j)
@@ -76,9 +83,13 @@ function solveSudoku(matrix) {
             squareMissing[square] = [];
             return;
           }
+    
+      return true;
     }
+    
+    return false;
   }
-  
+
   let findJoints = (i, j, square) => {
     let arr = [];
 
@@ -90,19 +101,38 @@ function solveSudoku(matrix) {
     return arr;
   }
 
-  let setJoints = () => {
-    for(let i = 0; i < 9; ++i) {
-        setRowMissing(i);
-        setColMissing(i);
-        setSquareMissing(i);
+  let findSectorsMissing = () => {  
+    let redo = true;
+    while(redo) {
+      redo = false;
+      for(let i = 0; i < 9; ++i) {
+          redo = setRowMissing(i) || redo;
+          redo = setColMissing(i) || redo;
+          redo = setSquareMissing(i) || redo;
+      }
     }
+  }
 
-    for(let i = 0; i < 9; ++i)
-      for(let j = 0; j < 9; ++j)
-        if(matrix[i][j] === 0)
-          joints[i][j] = findJoints(i, j, squarePos(i,j));
-        else
-          joints[i][j] = [];
+  let setCandidates = () => {
+    let redo = true;
+    while(redo) {
+      redo = false;
+    
+      findSectorsMissing();
+      
+      for(let i = 0; i < 9; ++i)
+        for(let j = 0; j < 9; ++j)
+          if(matrix[i][j] === 0) {
+            candidates[i][j] = findJoints( i, j, squarePos(i, j) );
+
+            if(candidates[i][j].length === 1) {
+              matrix[i][j] = candidates[i][j][0];
+              candidates[i][j] = [];
+              redo = true;
+            }
+          } else
+            candidates[i][j] = [];
+    }
   }
 
   let findUniq = (arr) => {
@@ -111,11 +141,12 @@ function solveSudoku(matrix) {
 
       if(another !== -1) {
         while(another !== -1) {
-          arr.splice(another, 1);
+          arr.splice(another + i + 1, 1);
           another = arr.slice(i + 1, arr.length).indexOf(arr[i]);
         }
 
         arr.splice(i, 1);
+        --i;
       }
     }
 
@@ -126,128 +157,129 @@ function solveSudoku(matrix) {
     let startRow = Math.floor(square / 3) * 3,
         startCol = (square % 3) * 3,
         arr = [];
-  
+
     for(let i = startRow; i < startRow + 3; ++i)
       for(let j = startCol; j < startCol + 3; ++j)
-        arr = arr.concat( joints[i][j] );
+        arr = arr.concat( candidates[i][j] );
 
     return findUniq(arr);
   }
 
   let insertSquareUniq = (square, uniqArr) => {
     if( uniqArr.length === 0)
-      return;
+      return false;
 
     let startRow = Math.floor(square / 3) * 3,
         startCol = (square % 3) * 3;
 
     for(let i = startRow; i < startRow + 3; ++i)
       for(let j = startCol; j < startCol + 3; ++j)
-        for(let n = uniqArr[0]; n < uniqArr.length; ++n)
-          if( joints[i][j].includes( uniqArr[n] ) )
+        for(let n = 0; n < uniqArr.length; ++n)
+          if( candidates[i][j].includes( uniqArr[n] ) )
             matrix[i][j] = uniqArr[n];
+    
+    return true;
   }
 
-  let lastSquareHero = () => {
-    for(let i = 0; i < 9; ++i)
-      insertSquareUniq( i, findSquareUniq(i) );
-  }
-  
   let findRowUniq = (row) => {
     let arr = [];
-  
-    for(let i = 0; i < 9; ++i)
-      arr = arr.concat( joints[row][i] );
 
+    for(let i = 0; i < 9; ++i)
+      arr = arr.concat( candidates[row][i] );
+    
     return findUniq(arr);
   }
 
   let insertRowUniq = (row, uniqArr) => {
     if( uniqArr.length === 0)
-      return;
+      return false;
 
     for(let i = 0; i < 9; ++i)
-      for(let n = uniqArr[0]; n < uniqArr.length; ++n)
-        if( joints[row][i].includes( uniqArr[n] ) )
+      for(let n = 0; n < uniqArr.length; ++n)
+        if( candidates[row][i].includes( uniqArr[n] ) )
           matrix[row][i] = uniqArr[n];
-  }
-
-  let lastRowHero = () => {
-    for(let i = 0; i < 9; ++i)
-      insertRowUniq( i, findRowUniq(i) );
+    
+    return true;
   }
 
   let findColUniq = (col) => {
     let arr = [];
-  
+
     for(let i = 0; i < 9; ++i)
-      arr = arr.concat( joints[i][col] );
+      arr = arr.concat( candidates[i][col] );
 
     return findUniq(arr);
   }
 
   let insertColUniq = (col, uniqArr) => {
     if( uniqArr.length === 0)
-      return;
+      return false;
 
     for(let i = 0; i < 9; ++i)
-      for(let n = uniqArr[0]; n < uniqArr.length; ++n)
-        if( joints[i][col].includes( uniqArr[n] ) )
+      for(let n = 0; n < uniqArr.length; ++n)
+        if( candidates[i][col].includes( uniqArr[n] ) )
           matrix[i][col] = uniqArr[n];
+    
+    return true;
   }
-  
-  let lastColHero = () => {
+
+  let lastHero = () => {
+    let redo = true;
+    while(redo) {
+      redo = false;
+      
+      setCandidates();
+      
+      for(let i = 0; i < 9; ++i) {
+        redo = insertSquareUniq( i, findSquareUniq(i) ) || redo;
+        redo = insertColUniq( i, findColUniq(i) ) || redo;
+        redo = insertRowUniq( i, findRowUniq(i) ) || redo;
+      }
+    }
+  }
+
+  let isCorrectFor = (candidate, row, col) => {
     for(let i = 0; i < 9; ++i)
-      insertColUniq( i, findColUniq(i) );
-  }
-  
-  let v = 250;
-  while(v--) {
-    setJoints();
-    lastSquareHero();
-    lastRowHero();
-    lastColHero();
+        if(matrix[i][col] === candidate || matrix[row][i] === candidate)
+          return false;
+
+    let [r, c] = squareStart( squarePos(row, col) );
+
+    for(let i = r; i < r + 3; ++i)
+      for(let j = c; j < c + 3; ++j)
+        if(matrix[i][j] === candidate)
+          return false;
+
+    return true;
   }
 
-  // console.log("arr=", rowMissing[0]);
+  let brutForce = () => {
+    for(let i = 0; i < 9; ++i) {
+      for(let j = 0; j < 9; ++j)
+        if(matrix[i][j] === 0) {
+          for(let n = 1; n <= 9; ++n) {
+            if( isCorrectFor(n, i, j) ) {
+              matrix[i][j] = n;
+              
+              if( brutForce() ) {
+                return true;
+              } else {
+                matrix[i][j] = 0;
+              }
+            }
+          }
 
-  // for(let i = 0; i < 9; ++i)
-  //   // for(let j = 0; j < 3; ++j)
-  //     console.log(joints[0][i]);
+          return false;
+        }
+    }
+
+    return true;
+  }
   
+  lastHero();
+  lastHero();
+  
+  brutForce();
+
   return matrix;
 }
-
-function isSolved(initial, sudoku) {
-  for (let i = 0; i < 9; i++) {
-    let [r,c] = [Math.floor(i/3)*3,(i%3)*3];
-    if (
-        (sudoku[i].reduce((s,v)=>s.add(v),new Set()).size != 9) ||
-        (sudoku.reduce((s,v)=>s.add(v[i]),new Set()).size != 9) ||
-        (sudoku.slice(r,r+3).reduce((s,v)=>v.slice(c,c+3).reduce((s,v)=>s.add(v),s),new Set()).size != 9)
-      ) return false;
-  }
-  return initial.every((row, rowIndex) => {
-    return row.every((num, colIndex) => {
-      return num === 0 || sudoku[rowIndex][colIndex] === num;
-    });
-  });
-}
-
-const initial = [ [6, 5, 0, 7, 3, 0, 0, 8, 0],
-                  [0, 0, 0, 4, 8, 0, 5, 3, 0],
-                  [8, 4, 0, 9, 2, 5, 0, 0, 0],
-                  [0, 9, 0, 8, 0, 0, 0, 0, 0],
-                  [5, 3, 0, 2, 0, 9, 6, 0, 0],
-                  [0, 0, 6, 0, 0, 0, 8, 0, 0],
-                  [0, 0, 9, 0, 0, 0, 0, 0, 6],
-                  [0, 0, 7, 0, 0, 0, 0, 5, 0],
-                  [1, 6, 5, 3, 9, 0, 4, 7, 0] ];
-const cop = initial.map(r => [...r]);
-let arr = solveSudoku(cop);
-
-console.log('is solved - ', isSolved(initial, arr));
-console.log('is change - ', (initial !== arr));
-
-arr.forEach( e => console.log("" + e) );
-// console.log("1" + arr);
